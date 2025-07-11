d4f8e120b827cc8f6cf1c97a9d9d1454c495d3c6
isreallee82 <isreallee82@gmail.com>
2025-07-12 00:50:52 +0100

    (feat) Update related member to customer

--- internal/serviceimpl/reward_service.go
+++ internal/serviceimpl/reward_service.go
@@ -79,7 +79,7 @@ func (s *rewardService) GetRewards(req request.GetRewardRequest) ([]models.Rewar
 	query = request.ApplyPaginationConditions(query, req.PaginationConditions)
 
 	// Fetch records with pagination
-	if err := query.Preload("RewardedMember").Preload("RelatedMember").Find(&rewards).Error; err != nil {
+	if err := query.Preload("RewardedCustomer").Preload("RelatedCustomer").Find(&rewards).Error; err != nil {
 		return nil, 0, fmt.Errorf("failed to fetch rewards: %w", err)
 	}
 
